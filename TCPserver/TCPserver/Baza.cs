6516fe82387f14a28a59fc85400b61a1c5b5337b
ianzonja <ianzonja@foi.hr>
2016-06-18 18:01:18 +0200

    BackendPocetak
    Cijeli TCP server koji ce rijesavati razmjenu podataka izmedu klijenta i
    servera, dekriptirati kriptiranu poruku te raditi s bazom.

---
+++ TCPserver/TCPserver/Baza.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCPserver
+{
+    class Baza
+    {
+        //SqlConnection konekcija = new SqlConnection(@"Data Source=DESKTOP-ENTPQER\SQLSERVERIAN;Initial Catalog=testBaza;Integrated Security=True");
+        //SqlCommand upit;
+        //SqlDataReader dr;
+        //int LogickiBrojac;
+        //string user = "admin";
+        //string password = "admin";
+        //public bool Logiran = false;
+        //public void KreirajUpit()
+        //{
+        //    upit = new SqlCommand("Select * from korisnik where username = '" + user + "' and password = '" + password + "'", konekcija);
+        //    konekcija.Open();
+        //    dr = upit.ExecuteReader();
+        //    if (dr.Read())
+        //    {
+        //        LogickiBrojac++;
+        //    }
+        //
+        //    if (LogickiBrojac == 1)
+        //    {
+        //        Console.WriteLine("Podaci tocni!");
+        //        Logiran = true;
+        //    }
+        //
+        //    else if (LogickiBrojac > 0)
+        //    {
+        //        Console.WriteLine("Podaci duplicirani");
+        //    }
+        //
+        //    else Console.WriteLine("POdaci krivo unseni");
+        //}
+        
+    }
+}
---
+++ TCPserver/TCPserver/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace TCPserver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string poruka = "";
+            TCPserver server = new TCPserver();
+            server.PokreniListener();
+        }
+    }
+}
