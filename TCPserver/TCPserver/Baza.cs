2d12317b8842c32f0687bbec6393a4d0786c8656
Ivan Užarević <ivan.uzarevic1995@gmail.com>
2016-07-02 17:17:48 +0200

    Dodana funkcija azuriraj() koja azurira na temelju upita podatke u bazi, možda će biti potrebne neke izmjene

--- TCPserver/TCPserver/Baza.cs
+++ TCPserver/TCPserver/Baza.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
@@ -42,6 +43,27 @@ namespace TCPserver
                     Console.WriteLine("No rows found.");
                 }
                 reader.Close();
+            }
+        }
+
+        public static void azuriraj(string upit)
+        {
+            SqlConnection connection = new SqlConnection("Server = tcp:crypto.database.windows.net,1433; Data Source = crypto.database.windows.net; Initial Catalog = CryptoBaza; Persist Security Info = False; User ID = ivauzarev; Password =crypto2101!; MultipleActiveResultSets = False; Encrypt = True; TrustServerCertificate = False; Connection Timeout = 30;");
+            connection.Open();
+
+            SqlCommand commandUpdate = new SqlCommand(upit);
+            try
+            {
+                commandUpdate.ExecuteNonQuery();
+                Console.WriteLine("Updated");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Not Updated");
+            }
+            finally
+            {
+                connection.Close();
             }
         }
 
