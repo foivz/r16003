cefcd6e187648b5732a0b047dd9c2579398a47af
Ivan Užarević <ivan.uzarevic1995@gmail.com>
2016-07-02 16:56:00 +0200

    Nadogradnja klase za rad sa bazom podataka, funkcija GeneriranjeOdgovora()

--- TCPserver/TCPserver/Baza.cs
+++ TCPserver/TCPserver/Baza.cs
@@ -10,6 +10,7 @@ namespace TCPserver
     class Baza
     {
         //uzarevic treba rijesiti ovu klasu da imamo odmah metode za spajanje na bazu i izvrsavanje upita koji dolaze iz klase UpitZaBazu
+
         private string upit;
 
         public Baza(string upit)
@@ -17,9 +18,32 @@ namespace TCPserver
             this.upit = upit;
         }
 
-        public void GeneriranjeOdgovora()
+        //funkcija za generiranje odgovora (provjerava korisnika u bazi)
+        public static void GeneriranjeOdgovora(string upit)
         {
+            SqlConnection connection = new SqlConnection("Server = tcp:crypto.database.windows.net,1433; Data Source = crypto.database.windows.net; Initial Catalog = CryptoBaza; Persist Security Info = False; User ID = ivauzarev; Password =crypto2101!; MultipleActiveResultSets = False; Encrypt = True; TrustServerCertificate = False; Connection Timeout = 30;");
+            using (connection)
+            {
+                SqlCommand command = new SqlCommand(upit, connection);
+                connection.Open();
 
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine("{0}\t{1}", reader.GetInt32(0),
+                            reader.GetString(1));
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No rows found.");
+                }
+                reader.Close();
+            }
         }
+
     }
 }
