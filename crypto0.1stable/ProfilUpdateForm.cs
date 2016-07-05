f76bfc36dfba9a8038437895c6ad7353c661c38d
Ivan Užarević <ivan.uzarevic1995@gmail.com>
2016-07-06 00:18:48 +0200

    Dodana forma za update profila

---
+++ crypto0.1stable/ProfilUpdateForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace crypto0._1stable
+{
+    public partial class ProfilUpdateForm : Form
+    {
+        public ProfilUpdateForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
