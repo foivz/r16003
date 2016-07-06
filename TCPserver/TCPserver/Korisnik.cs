214ad18c5a97be497d9061729ee966e498376f73
Ivan Užarević <ivan.uzarevic1995@gmail.com>
2016-07-06 02:34:53 +0200

    Funkcije za obradu podataka o korisniku aplikacije na server razini

--- TCPserver/TCPserver/Korisnik.cs
+++ TCPserver/TCPserver/Korisnik.cs
@@ -14,5 +14,45 @@ namespace TCPserver
         public string DatumRodjenja { get; set; }
         public int RazinaPristupa { get; set; } //moze biti 1,2,3 (ovisno je li admin,moderator,korisnik)
         public bool Aktivnost { get; set; }
+
+        public void Dodaj(string username,string password,string email,string datum,int razina,bool aktivnost)
+        {
+            Username = username;
+            Password = password;
+            Email = email;
+            DatumRodjenja = datum;
+            RazinaPristupa = razina;
+            Aktivnost = aktivnost;
+        }
+
+        public string GetUsername()
+        {
+            return this.Username;
+        }
+
+        public string GetPassword()
+        {
+            return this.Password;
+        }
+
+        public string GetEmail()
+        {
+            return this.Email;
+        }
+
+        public string GetDatumRodjenja()
+        {
+            return this.DatumRodjenja;
+        }
+
+        public int GetRazinaPristupa()
+        {
+            return this.RazinaPristupa;
+        }
+
+        public bool GetAktivnost()
+        {
+            return this.Aktivnost;
+        }
     }
 }
